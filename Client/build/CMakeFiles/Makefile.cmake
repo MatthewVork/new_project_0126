ee090cdb484a28a78e2211cae572eae2b53e6a6c
MatthewVork <wzy1845089791@gmail.com>
2026-01-31 01:32:42 +0800

    修改注释

--- Client/build/CMakeFiles/Makefile.cmake
+++ Client/build/CMakeFiles/Makefile.cmake
@@ -7,7 +7,7 @@ set(CMAKE_DEPENDS_GENERATOR "Unix Makefiles")
 # The top level Makefile was generated from the following files:
 set(CMAKE_MAKEFILE_DEPENDS
   "CMakeCache.txt"
-  "/mnt/c/Users/Matthew/Desktop/new_project_0126/Client/CMakeLists.txt"
+  "/mnt/c/Users/18450/OneDrive/Desktop/new_project_0126/Client/CMakeLists.txt"
   "CMakeFiles/3.28.3/CMakeCCompiler.cmake"
   "CMakeFiles/3.28.3/CMakeSystem.cmake"
   "/usr/share/cmake-3.28/Modules/CMakeCCompiler.cmake.in"
--- Client/build/CMakeFiles/game_client.dir/lvgl/src/misc/lv_log.c.o.d
+++ Client/build/CMakeFiles/game_client.dir/lvgl/src/misc/lv_log.c.o.d
@@ -1,8 +1,8 @@
 CMakeFiles/game_client.dir/lvgl/src/misc/lv_log.c.o: \
- /mnt/c/Users/Matthew/Desktop/new_project_0126/Client/lvgl/src/misc/lv_log.c \
+ /mnt/c/Users/18450/OneDrive/Desktop/new_project_0126/Client/lvgl/src/misc/lv_log.c \
  /usr/include/stdc-predef.h \
- /mnt/c/Users/Matthew/Desktop/new_project_0126/Client/lvgl/src/misc/lv_log.h \
- /mnt/c/Users/Matthew/Desktop/new_project_0126/Client/lvgl/src/misc/../lv_conf_internal.h \
+ /mnt/c/Users/18450/OneDrive/Desktop/new_project_0126/Client/lvgl/src/misc/lv_log.h \
+ /mnt/c/Users/18450/OneDrive/Desktop/new_project_0126/Client/lvgl/src/misc/../lv_conf_internal.h \
  /usr/lib/gcc/x86_64-linux-gnu/13/include/stdint.h /usr/include/stdint.h \
  /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
  /usr/include/features.h /usr/include/features-time64.h \
@@ -19,6 +19,6 @@ CMakeFiles/game_client.dir/lvgl/src/misc/lv_log.c.o: \
  /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
  /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
  /usr/include/x86_64-linux-gnu/bits/stdint-least.h \
- /mnt/c/Users/Matthew/Desktop/new_project_0126/Client/lvgl/src/misc/../lv_conf_kconfig.h \
- /mnt/c/Users/Matthew/Desktop/new_project_0126/Client/./lv_conf.h \
- /mnt/c/Users/Matthew/Desktop/new_project_0126/Client/lvgl/src/misc/lv_types.h
+ /mnt/c/Users/18450/OneDrive/Desktop/new_project_0126/Client/lvgl/src/misc/../lv_conf_kconfig.h \
+ /mnt/c/Users/18450/OneDrive/Desktop/new_project_0126/Client/./lv_conf.h \
+ /mnt/c/Users/18450/OneDrive/Desktop/new_project_0126/Client/lvgl/src/misc/lv_types.h
